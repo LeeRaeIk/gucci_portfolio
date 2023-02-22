f83e8fda1afdce56015564fb52978153cfd7b6e8
LeeRaeIk <dlfdolr@daum.net>
2023-02-22 16:11:23 +0900

    gucci_prtfolio

---
+++ js/custom.js
@@ -0,0 +1,135 @@
+const swiper = new Swiper('.one', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type:'bullets' ,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  grabCursor: false, //스와이프 시 마우스 포인터 모양 변경
+  slidesPerView: 'auto', // css에 지정한 슬라이더의 크리를 반영
+  centeredSlides : true, // 슬라이더 패널을을 항상 가운데로 반환
+  speed: 1000, //슬라이더 이동 속도 지정
+  effect : 'slide', // 이팩트 효과
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  mousewheel: {
+    invert: false,
+    
+  },
+});
+
+const swiper2 = new Swiper('.two', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  grabCursor: true, //스와이프 시 마우스 포인터 모양 변경
+  slidesPerView: 'auto', // css에 지정한 슬라이더의 크리를 반영
+  centeredSlides : true, // 슬라이더 패널을을 항상 가운데로 반환
+  speed: 1500, //슬라이더 이동 속도 지정
+  effect : 'slide', // 이팩트 효과
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+const swiper3 = new Swiper('.three', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  grabCursor: true, //스와이프 시 마우스 포인터 모양 변경
+  slidesPerView: 'auto', // css에 지정한 슬라이더의 크리를 반영
+  centeredSlides : true, // 슬라이더 패널을을 항상 가운데로 반환
+  speed: 1500, //슬라이더 이동 속도 지정
+  effect : 'slide', // 이팩트 효과
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+const swiper4 = new Swiper('.four', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  grabCursor: true, //스와이프 시 마우스 포인터 모양 변경
+  slidesPerView: 'auto', // css에 지정한 슬라이더의 크리를 반영
+  centeredSlides : true, // 슬라이더 패널을을 항상 가운데로 반환
+  speed: 1500, //슬라이더 이동 속도 지정
+  effect : 'slide', // 이팩트 효과
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+const swiper5 = new Swiper('.five', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  grabCursor: true, //스와이프 시 마우스 포인터 모양 변경
+  slidesPerView: 'auto', // css에 지정한 슬라이더의 크리를 반영
+  centeredSlides : true, // 슬라이더 패널을을 항상 가운데로 반환
+  speed: 1500, //슬라이더 이동 속도 지정
+  effect : 'slide', // 이팩트 효과
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
